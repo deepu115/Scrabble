b9e910c9a5f31d7bb9e3e182278455eef7cb250b
Dileep <dileepvunnam@gmail.com>
2023-07-03 12:55:13 +0100

    first test passed

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,2 +1,12 @@
 public class Scrabble {
+    String word;
+    public Scrabble(String word){
+        this.word = word;
+    }
+     public int score() {
+         if (this.word.equals("")) {
+         }
+         return 0;
+     }
+
 }
